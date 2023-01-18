561bdd8eba75bf80d7ce7f9a982dc31e39183cb8
dev-rashidul <rashidul.contact@gmail.com>
2023-01-18 16:21:22 +0600

    Hero Section Done

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// $(".hero-slider").owlCarousel({
+//     loop: true,
+//     rtl: true,
+//     autoplay:true,
+//     autoplayTimeout:3000,
+//     smartSpeed:450,
+//     animateOut: 'slideOutDown',
+//     animateIn: 'flipInX',
+//     nav: true,
+//     dots: false,
+//     navText:['<i class="fa-solid fa-chevron-right"></i>','<i class="fa-solid fa-chevron-left"></i>'],
+//     responsive: {
+//       0: {
+//         items: 1,
+//       },
+//     },
+//   });
+
+
+$('.hero-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true,
+
+  });
+          
