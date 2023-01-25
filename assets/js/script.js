76a23607b0f5c840439b46c7aefe789814ebb400
dev-rashidul <rashidul.contact@gmail.com>
2023-01-25 12:41:58 +0600

    Coach Page Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,3 +141,32 @@ $('.articles-slider').slick({
   prevArrow: '<span class="slick-prev"><i class="fa-solid fa-chevron-left"></i></span>',
   nextArrow: '<span class="slick-next"><i class="fa-solid fa-chevron-right"></i></span>',
 });
+
+// Coach Page Video Slider JavaScript
+
+$('.video-slider').slick({
+  arrows: true,
+  dots: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  rtl: true,
+  responsive: [
+
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ],
+  prevArrow: '<span class="slick-prev"><i class="fa-solid fa-chevron-left"></i></span>',
+  nextArrow: '<span class="slick-next"><i class="fa-solid fa-chevron-right"></i></span>',
+});
